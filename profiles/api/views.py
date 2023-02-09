cd816ca0497a926afe8863313c57bd8015770f85
Fateme Fallah <59208100+fatemefallah@users.noreply.github.com>
2023-02-09 15:41:46 +0330

    first apiview

--- profiles/api/views.py
+++ profiles/api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class Hello(APIView):
+    """first api view"""
+
+    def get(self, requset, format=None):
+        """Rerutns a list"""
+        apiview = ["test1", "test2", "test3"]
+
+        return Response({"message": "hello", "apiview": apiview})
